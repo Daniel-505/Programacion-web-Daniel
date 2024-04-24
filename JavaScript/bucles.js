7ba6bc64270cdd607d59a325d34f100a77f025f7
Daniel-505 <fericos190@gmail.com>
2024-04-24 16:49:14 -0300

    modifique las cartas de pokemon

---
+++ JavaScript/bucles.js
@@ -0,0 +1,11 @@
+const arrays = [1,2,3,]
+let contador = 0
+
+while(arrays.length < 2){
+    console.log('hola');
+    contador ++;
+}
+
+for (let i = 0; i < arrays.length; i++){
+    console.log("arrays", arrays[i])
+}
