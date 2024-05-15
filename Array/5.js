3500b6406be17aa125d596474450c2ec12ed93ee
Daniel-505 <fericos190@gmail.com>
2024-05-15 16:52:45 -0300

    muejjeje

---
+++ Array/5.js
@@ -0,0 +1,5 @@
+const numbers = [5, -10, -2, -25, -7, 1]
+
+function sortAbsoluteNumbers(numbers) {
+  // código 
+  }
---
+++ Array/iteracion-3.js
@@ -0,0 +1,11 @@
+const lista = [1,2,4,5,5,6,6,7] 
+
+function sumarPares(numeros) {
+    const nombreCliente = pedidos.shift();
+    pedidos.unshift("bebida");
+    pedidos.push(nombreCliente);
+  // código 
+}
+
+
+
