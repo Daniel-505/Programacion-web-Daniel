a4adec3bb1f94f7d767af323e96711b53d8b3b67
Daniel-505 <fericos190@gmail.com>
2024-05-22 16:44:55 -0300

    solo ejercicio 1

---
+++ Objetos/Ejercicio_1..js
@@ -0,0 +1,12 @@
+const persona = {
+    name: "Daniel",
+    lasName: "Terrazas",
+    isWorking: true,
+    age: 18,
+    dog: ["odi"],
+    loros: ["mimi", "eve", "ghost", "blanquito"],
+    walk: function() {
+        console.log("estoy caminando");
+    },
+};
+console.log(persona)
