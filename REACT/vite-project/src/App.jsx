91c2e67204373295b8cc57d3a31ce7437cb20b6a
Daniel-505 <fericos190@gmail.com>
2024-09-18 16:56:45 -0300

    agregue el blog

--- REACT/vite-project/src/App.jsx
+++ REACT/vite-project/src/App.jsx
@@ -1,28 +1,40 @@
-import React from 'react';
-import './index.css'; // Asegúrate de que el archivo CSS esté en la misma carpeta
+import React from "react";
+import "./index.css"; // Asegúrate de que el archivo CSS esté en la misma carpeta
 
 function App() {
+  const images = [
+    { src: "public/fotos/IMG_8654.JPG", description: "Descripción 1" },
+    { src: "public/fotos/IMG_8654.JPG", description: "Descripción 1" },
+    { src: "public/fotos/IMG_8654.JPG", description: "Descripción 1" },
+    { src: "public/fotos/IMG_8654.JPG", description: "Descripción 1" },
+    { src: "public/fotos/IMG_8654.JPG", description: "Descripción 1" },
+    { src: "public/fotos/IMG_8654.JPG", description: "Descripción 1" },
+    { src: "public/fotos/IMG_8654.JPG", description: "Descripción 1" },
+    { src: "public/fotos/IMG_8654.JPG", description: "Descripción 1" },
+    { src: "public/fotos/IMG_8654.JPG", description: "Descripción 1" },
+    { src: "public/fotos/IMG_8654.JPG", description: "Descripción 1" },
+    { src: "public/fotos/IMG_8654.JPG", description: "Descripción 1" },
+    { src: "public/fotos/IMG_8654.JPG", description: "Descripción 1" },
+    { src: "public/fotos/IMG_8654.JPG", description: "Descripción 1" },
+    { src: "public/fotos/IMG_8654.JPG", description: "Descripción 1" },
+    { src: "public/fotos/IMG_8654.JPG", description: "Descripción 1" },
+    { src: "public/fotos/IMG_8654.JPG", description: "Descripción 1" },
+  ];
+
   return (
     <div className="body">
       <header className="header">
         <h1>Portfolio de Daniel</h1>
       </header>
-      <nav className="nav">
-        <a href="https://www.instagram.com/dxt7788/" className="social-link" aria-label="Daniel's Instagram Profile">Instagram</a>
-      </nav>
       <section className="content">
-        <h2>Foto y Videos</h2>
-        <ul className="info-list">
-          <li>Nose qué poner acá</li>
-        </ul>
-        <main className="main-content">
-          <div className="image-container">
-            <img src="/fotos/IMG_8654.JPG" alt="Descripción de la imagen" />
-            <div className="text-box">
-              <p>Me ayudé con lo que anoté en mi cuaderno muejejej, tenía que hacerlo con REACT pero lo hice con HTML porque es más cómodo para mí muejejjeje</p>
+        <div className="gallery">
+          {images.map((image, index) => (
+            <div className="image-item" key={index}>
+              <img src={image.src} alt={image.description} />
+              <p>{image.description}</p>
             </div>
-          </div>
-        </main>
+          ))}
+        </div>
       </section>
     </div>
   );
